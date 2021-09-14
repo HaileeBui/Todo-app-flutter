b605baee7ea1272cfe20b7718cad8380c4601e97
Misty Bui <mishtybui@gmail.com>
2021-09-14 10:56:12 +0300

    Tab bar created

---
+++ lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0xFF212128);
+Color pinkColor = new Color(0xFFF06292);
+
+TextStyle intrayTitleStyle = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 40);
